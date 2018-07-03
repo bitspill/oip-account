4a2b0a05e1794921d41e145d2e73783678aa95fb
Sky Young <sky@alexandria.io>
2018-07-03 16:48:27 -0700

    Run OIP-Keystore server for tests

--- test/KeystoreStorageAdapter.test.js
+++ test/KeystoreStorageAdapter.test.js
@@ -1,3 +1,5 @@
+var OIPKeystore = require('oip-keystore')
+
 var KeystoreStorageAdapter = require("../lib/KeystoreStorageAdapter")
 
 var isValidIdentifier = require("../lib/util").isValidIdentifier;
@@ -7,17 +9,23 @@ var test_account_data = { wallet: "test data", something: { subthing: { hi: "hel
 var keystore_url = "http://localhost:9196"
 var password = "password"
 
+var server
+
+beforeAll((done) => {
+	server = OIPKeystore.listen(9196, done)
+})
+
 test("Create new Account & Load", (done) => {
-	// var adapter = new KeystoreStorageAdapter(undefined, password, keystore_url)
+	var adapter = new KeystoreStorageAdapter(undefined, password, keystore_url)
 
-	// adapter.create(test_account_data).then((id) => {
-	// 	test_account_data.identifier = id;
+	adapter.create(test_account_data).then((id) => {
+		test_account_data.identifier = id;
 
-	// 	adapter.load().then((account_data) => {
-	// 		expect(account_data).toEqual(test_account_data);
-	// 		expect(isValidIdentifier(account_data.identifier)).toBe(true);
-	// 		done()
-	// 	})
-	// })
-	expect(true).toBe(true)
+		adapter.load().then((account_data) => {
+			expect(account_data).toEqual(test_account_data);
+			expect(isValidIdentifier(account_data.identifier)).toBe(true);
+			server.close()
+			done()
+		})
+	})
 })
