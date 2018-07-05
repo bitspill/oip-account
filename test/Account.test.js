b9798e1f2633764561a4486e966b4a4b7193d1cb
Sky Young <sky@alexandria.io>
2018-07-05 15:17:38 -0700

    Update Tests

--- test/Account.test.js
+++ test/Account.test.js
@@ -1,7 +1,7 @@
-var Account = require("../lib/Account");
+var Account = require("../src/Account");
 
 test("Create new Account NO STORAGE!", (done) => {
-	var acc = new Account(undefined, undefined, {discover: false});
+	var acc = new Account(undefined, undefined, {store_memory: true, discover: false});
 
 	acc.create().then((account_info) => {
 		expect(account_info.identifier).toBeDefined()
@@ -11,10 +11,21 @@ test("Create new Account NO STORAGE!", (done) => {
 })
 
 test("Create Account from Mnemonic NO STORAGE!", (done) => {
-	var acc = new Account("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about", undefined, {discover: false});
+	var acc = new Account("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about", undefined, {store_memory: true, discover: false});
 
 	acc.login().then((account_info) => {
-		expect(account_info.identifier).toBe("")
+		expect(account_info.identifier).toBeDefined()
+		expect(account_info.wallet.mnemonic).toBe("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
+		expect(acc.wallet.getMnemonic()).toBe("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
+		done()
+	})
+})
+
+test("Create Account from Mnemonic (Storage)", (done) => {
+	var acc = new Account("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about", "password", {discover: false});
+
+	acc.create().then((account_info) => {
+		expect(account_info.identifier).toBeDefined()
 		expect(account_info.wallet.mnemonic).toBe("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
 		expect(acc.wallet.getMnemonic()).toBe("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
 		done()
@@ -22,7 +33,7 @@ test("Create Account from Mnemonic NO STORAGE!", (done) => {
 })
 
 test("Can't find account if doesn't exist", (done) => {
-	var acc = new Account("test@me.com", "password", {store_local: true, discover: false})
+	var acc = new Account("test@me.com", "password", {discover: false})
 
 	acc.login().catch((error) => {
 		expect(error).toEqual(new Error("Account Not Found!"))
@@ -30,13 +41,41 @@ test("Can't find account if doesn't exist", (done) => {
 	})
 })
 
-test("", (done) => {
-	var acc = new Account("test@me.com", "password", {store_local: true, discover: false})
+test("Create Account (email)", async (done) => {
+	var acc = new Account("test@me.com", "password", {discover: false})
 
-	acc.create().then((account_info) => {
-		expect(account_info.identifier).toBeDefined()
-		expect(account_info.wallet.mnemonic).toBeDefined()
-		done()
-	})
+	var account_info = await acc.create()
+
+	expect(account_info.identifier).toBeDefined()
+	expect(account_info.email).toBe("test@me.com")
+	expect(account_info.wallet.mnemonic).toBeDefined()
+
+	var acc2 = new Account("test@me.com", "password", {discover: false})
+
+	var account_info_2 = await acc2.login()
+
+	expect(account_info_2).toEqual(account_info)
+
+	account_info_2.settings.displayNSFW = false;
+
+	var account_info_3 = await acc2.setSetting("displayNSFW", false)
+
+	expect(account_info_3).toEqual(account_info_2)
+
+	expect(acc2.getSetting("displayNSFW")).toBe(false)
+	expect(acc2.getSetting("nonSetting")).toBe(undefined)
+
+	done()
+})
+
+test("Create Account (no email)", async (done) => {
+	var acc = new Account(undefined, "password", {discover: false})
+
+	var account_info = await acc.create()
+
+	expect(account_info.identifier).toBeDefined()
+	expect(account_info.email).toBeUndefined()
+	expect(account_info.wallet.mnemonic).toBeDefined()
+	done()
 })
 
--- test/LocalStorageAdapter.test.js
+++ test/LocalStorageAdapter.test.js
@@ -3,17 +3,58 @@ var LocalStorageAdapter = require("../lib/LocalStorageAdapter")
 var isValidIdentifier = require("../lib/util").isValidIdentifier;
 
 var test_account_data = { wallet: "test data", something: { subthing: { hi: "hello!"}}}
+var test_account_data_2 = { wallet: "test data", something: { subthing: { hi: "hello!"}}}
 
-test("Create new Account & Load", (done) => {
-	var adapter = new LocalStorageAdapter()
+var password = "password"
 
-	adapter.create(test_account_data).then((id) => {
-		test_account_data.identifier = id;
+test("Create new Account & Load (no email)", async (done) => {
+	var adapter = new LocalStorageAdapter(undefined, password)
 
-		adapter.load().then((account_data) => {
-			expect(account_data).toEqual(test_account_data);
-			expect(isValidIdentifier(account_data.identifier)).toBe(true);
-			done()
-		})
-	})
+	var id = await adapter.create(test_account_data)
+
+	test_account_data.identifier = id;
+
+	var adapter2 = new LocalStorageAdapter(id, password)
+
+	var account_data = await adapter2.load()
+
+	test_account_data.shared_key = account_data.shared_key;
+
+	expect(account_data).toEqual(test_account_data);
+	expect(isValidIdentifier(account_data.identifier)).toBe(true);
+
+	test_account_data.wallet = { bitcoin: "test-key" }
+
+	var saved_data = await adapter2.save(test_account_data)
+	var saved_account_data = await adapter2.load()
+
+	expect(saved_account_data).toEqual(test_account_data)
+
+	done()
+})
+
+test("Create new Account & Load (email)", async (done) => {
+	var adapter = new LocalStorageAdapter(undefined, password)
+
+	var id = await adapter.create(test_account_data_2, "test@example.com")
+	test_account_data_2.identifier = id;
+
+	var adapter2 = new LocalStorageAdapter("test@example.com", password)
+
+	var account_data = await adapter2.load()
+
+	test_account_data_2.shared_key = account_data.shared_key;
+	test_account_data_2.email = account_data.email;
+
+	expect(account_data).toEqual(test_account_data_2);
+	expect(isValidIdentifier(account_data.identifier)).toBe(true);
+
+	test_account_data_2.wallet = { litecoin: "test-key" }
+
+	var saved_data = await adapter2.save(test_account_data_2)
+	var saved_account_data = await adapter2.load()
+
+	expect(saved_account_data).toEqual(test_account_data_2)
+
+	done()
 })
