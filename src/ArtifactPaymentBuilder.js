980611846c9a22595fe0889249a399e77bf987af
Ryan <ryan.chacon@alexandria.io>
2018-07-05 21:38:21 -0700

    calculate xRates/balances/usableCoins
    function successfully runs through, grabs exchangeRates, balances, does conversion costs, and gives a list of usable coins that are apt to pay the amount(s) given

--- src/ArtifactPaymentBuilder.js
+++ src/ArtifactPaymentBuilder.js
@@ -1,17 +1,17 @@
 const Exchange = require("oip-exchange-rate");
-// var { ArtifactFile } = require("oip-index");
+var { ArtifactFile } = require("oip-js");
 
 class ArtifactPaymentBuilder {
 	/**
 	 * Create a new ArtifactPaymentBuilder
 	 * @param  {Wallet} wallet   - A live OIP-HDMW logged in wallet
-	 * @param  {string} type     - The type of the purchase, either `tip`, `view`, or `buy`
 	 * @param  {Artifact} artifact - The Artifact related to the Payment you wish to make
-	 * @param  {ArtifactFile|number} amount	- The amount you wish to pay (`tip`), or the ArtifactFile you wish to pay for (`view` & `buy`)
+     * @param  {string} type     - The type of the purchase, either `tip`, `view`, or `buy`
+     * @param  {ArtifactFile|number} amount	- The amount you wish to pay (`tip`), or the ArtifactFile you wish to pay for (`view` & `buy`)
 	 * @param  {string} [fiat]   - The Fiat you wish to `tip` in (if amount was a number and NOT an ArtifactFile)
 	 * @return {ArtifactPaymentBuilder}          [description]
 	 */
-	constructor(wallet, type, artifact, amount, fiat){
+	constructor(wallet, artifact, type, amount, fiat){
         this._wallet = wallet;
         this._type = type;
         this._artifact = artifact;
@@ -28,102 +28,126 @@ class ArtifactPaymentBuilder {
 	 * @return {Promise<Transaction>} Returns a Promise that resolves to the payment transaction, or rejects if there was an error
 	 */
 	async pay(){
-	    let rates, balances, artifactFileCost;
-        let fiat = this._fiat;
-
+	    console.log("Beginning Payment")
+	    let paymentAmount, fiat;
+	    fiat = this._fiat;
         // Get ArtifactFile Cost and Artifact Fiat
 	    if (this._amount instanceof ArtifactFile) {
-	        console.log(this._amount)
 	        switch (this._type) {
                 case "buy":
-                    artifactFileCost = this._amount.getSuggestedBuyCost();
+                    paymentAmount = this._amount.getSuggestedBuyCost();
                     break;
                 case "play":
-                    artifactFileCost = this._amount.getSuggestedPlayCost();
+                    paymentAmount = this._amount.getSuggestedPlayCost();
                     break;
                 case "tip":
-                    artifactFileCost = this._amount;
+                    paymentAmount = this._amount;
                     break;
             }
-        } else {artifactFileCost = this._amount}
+            //@ToDo: set default
+        } else {paymentAmount = this._amount}
 
+        console.log(`paymentAmount: ${paymentAmount} and Fiat: ${this._fiat}`);
+
+        let coinsToFetch = [];
+        // ------ @ToDo: use this when you get a valid 42 artifact
+        // for (let coin of this.getPaymentAddresses()) {
+	     //    for (let coin in addressPairs[addr]) {
+        //         coinsToFetch.push(coin)
+        //     }
+        // }
+
+        // ------ @ToDo: then delete this
+        let addressPairs = [{flo: "FLZXRaHzVPxJJfaoM32CWT4GZHuj2rx63k"}];
+        for (let addr in addressPairs) {
+            for (let coin in addressPairs[addr]) {
+                coinsToFetch.push(coin)
+            }
+        }
+
+        console.log(`Coins to fetch: ${coinsToFetch}`)
 
         // Get percentages to be paid out to Platforms and Influencers (don't worry about this for now)
 			
 		// Calculate crypto cost based on the exchange rate for the Fiat (using oip-exchange-rate)
+        let exchangeRates;
         try {
-	        rates = await this.getExchangeRates(this._fiat);
-	    } catch (err) {
+            exchangeRates = await this.getExchangeRates(this._fiat, coinsToFetch);
+            console.log(`exchangeRates: ${JSON.stringify(exchangeRates, null, 4)}`)
+        } catch (err) {
 	        console.log(`Error on getExchangeRates: ${err}`)
 	    };
 
-        // Check Balances of Cryptocurrencies
+        // Check Coin Balances
+        let coinBalances;
         try {
-            balances = await this.getBalances()
+            coinBalances = await this.getBalances(coinsToFetch)
+            console.log(`myCoinBalances: ${JSON.stringify(coinBalances, null, 4)}`)
+
         } catch (err) {
             console.log(`Error on getBalances: ${err}`)
-        };
+        }
 
         //exchange rate / file cost
-        let fileCosts = {};
-        for (let coin in rates) {
-            if (rates[coin].fiat) {
-                fileCosts[coin] = rates[coin].fiat / artifactFileCost
-            } else if (rates[coin].error) {
-                fileCosts[coin] = `Error finding rate: ${rates[coin].error}`
+        let conversionPrices = {};
+        for (let coin in exchangeRates) {
+            if (typeof exchangeRates[coin][fiat] === "number") {
+                conversionPrices[coin] = paymentAmount / exchangeRates[coin][fiat];
+                console.log(`${conversionPrices[coin]} = ${paymentAmount} / ${exchangeRates[coin][fiat]}`)
             }
         }
-        console.log("fileCosts: ", fileCosts)
-
-        fileCosts, balances
-        for (var coin of balances) {
-
-            if (balances[coin].balance < fileCosts[coin]) {
-
-            }
-        }
+        console.log("conversionPrices: ", conversionPrices);
 
         // If not enough balance
+        //reject(new Error("Not Enough Balance!"))
+        let usableCoins = [];
+        for (let coin in coinBalances) {
+            if (coinBalances[coin] && conversionPrices[coin]) {
+                if (coinBalances[coin] >= conversionPrices[coin]) {
+                    usableCoins.push(coin)
+                } else console.log(`Balance not enough: ${coinBalances[coin]} <= ${conversionPrices[coin]}`)
+            }
+        }
+        console.log(`Usable coins: ${usableCoins}`)
+        // Pick crypto(s) to pay with
 
-				//reject(new Error("Not Enough Balance!"))
+        // Send the payment in that crypto to the User (using this.wallet)
 
-				// Select which cryptocurrency to use
-				
-				// Send the payment in that crypto to the User (using this.wallet)
-				
-				// Save Transaction to `paymentHistory` if payment went through successfully
+        // Save Transaction to `paymentHistory` if payment went through successfully
 
-	}
+    }
 
     /**
      * Get balances for each coin
+     * @param  {array} [coinArray]    - An array of coins you want to get balances for. If no coins are given, an array of all available coins will be used.
      * @return {object}
-     *   @example
+     * @example
      * {
-     *      "flo": {balance: 216},
-     *      "btc": {error: "Error text"},
-     *      "ltc": {balance: 333}
+     *      "flo": 216,
+     *      "btc": "error fetching balance",
+     *      "ltc": 333
      * }
      */
-     async getBalances() {
-            const coins = this._wallet.getCoins();
+     async getBalances(coinArray) {
+            const coins = coinArray || Object.key(this._wallet.getCoins());
+            let coinClass = this._wallet.getCoins();
 
             let coinPromises = {};
             let balances = {};
 
-            for (let coin in coins) {
+            for (let coin of coins) {
                 coinPromises[coin] = {};
                 try {
-                    coinPromises[coin] = coins[coin].getBalance({discover: true})
+                    coinPromises[coin] = coinClass[coin].getBalance({discover: true})
                 } catch (err) {
-                    coinPromises[coin] = "error fetching promise";
+                    coinPromises[coin] = "promise error fetching balance";
                     console.log(`Error on ${coin}: ${err}`)
                 }
             }
 
             for (let coin in coinPromises) {
                 try {
-                    let balance = await coinPromises[coin]
+                    let balance = await coinPromises[coin];
                     balances[coin] = {};
                     balances[coin] = balance
                 } catch (err) {
@@ -142,7 +166,7 @@ class ArtifactPaymentBuilder {
      * Calculate the exchange rate between a fiat currency and a cryptocurrency
      * @param  {string} fiat     - The fiat currency you wish to check against
      * @param  {array} [coinArray]    - An array of coins you want to get exchange rates for. If no coins are given, an array of all available coins will be used.
-     * @return {object}
+     * @return {object} rates
      * @example
      * {
      *      "flo": {"usd": expect.any(Number)},
--- test/ArtifactPaymentBuilder.test.js
+++ test/ArtifactPaymentBuilder.test.js
@@ -1,64 +1,93 @@
 var ArtifactPaymentBuilder = require("../lib/ArtifactPaymentBuilder");
 var { Wallet } = require("oip-hdmw");
+var {Artifact} = require("oip-js");
 
-const wallet = new Wallet("00000000000000000000000000000000", {discover: false})
-const APB = new ArtifactPaymentBuilder(wallet);
-
-test("APB, getExchangeRates(): No Coin Parameters", async () => {
-    expect.assertions(1);
-    await expect(APB.getExchangeRates("usd")).resolves.toMatchObject(
-        {
-            "flo": {"usd": expect.any(Number)},
-            "bitcoin": {"usd": expect.any(Number)},
-            "litecoin": {"usd": expect.any(Number)}
+var artifact = new Artifact({
+    "oip042": {
+        "artifact": {
+            "floAddress": "FLZXRaHzVPxJJfaoM32CWT4GZHuj2rx63k",
+            "type": "Image",
+            "info": {
+                "title": "Example Artifact",
+                "description": "Example Artifact Description"
+            },
+            "storage": {
+                "network": "IPFS",
+                "files": [
+                    {
+                        "fname": "my_cool_picture.png",
+                        "fsize": 23591,
+                        "type": "Image"
+                    }
+                ],
+                "location": "QmQh7uTC5YSinJG2FgWLrd8MYSNtr8G5JGAckR5ARwmyET"
+            },
+            "payment": {
+                "addresses": [{flo: 216}]
+            },
+            "timestamp": 1508188263,
+            "signature": "IAiCzx8ICjAKoj98yw5VwKLCzIuAGM1fVIewZjC/PrBHVkUsl67R2Pv0Eu1fFaWsoONmVc1lZA+lpmQ4/dGVG6o="
         }
-    )
-    // uncomment to check exchange rates:
-    // await expect(APB.getExchangeRates("usd")).resolves.toBe()
-}, 10000)
-
-test("APB, getExchangeRates(): One Coin Parameter", async () => {
-    expect.assertions(1);
-    await expect(APB.getExchangeRates("usd", ["flo"])).resolves.toMatchObject(
-        {
-            "flo": {"usd": expect.any(Number)}
-        }
-    )
-}, 10000)
-
-test("APB, getExchangeRates(): Multiple Coin Parameters", async () => {
-    expect.assertions(1);
-    await expect(APB.getExchangeRates("usd", ["flo", "bitcoin", "litecoin"])).resolves.toMatchObject(
-        {
-            "flo": {"usd": expect.any(Number)},
-            "bitcoin": {"usd": expect.any(Number)},
-            "litecoin": {"usd": expect.any(Number)}
-        }
-    )
+    }
 })
+const wallet = new Wallet("00000000000000000000000000000000", {discover: false})
+const APB = new ArtifactPaymentBuilder(wallet, artifact, "view", .00012);
 
-test("APB, getBalances(): check to see if balances resolved", async () => {
-    // expect.assertions(3);
-    let balances = await APB.getBalances({discover: false});
-
-    let bitcoinResolved = false;
-    let floResolved = false;
-    let ltcResolved = false;
-    console.log("Balances, ", balances)
-    if (typeof balances["bitcoin"] === "number" || typeof balances["bitcoin"] === "string") {
-        bitcoinResolved = true;
-    }
-    if (typeof balances["flo"] === "number" || typeof balances["flo"] === "string") {
-        floResolved = true;
-    }
-    if (typeof balances["litecoin"] === "number" || typeof balances["litecoin"] === "string") {
-        ltcResolved = true;
-    }
-
-    expect(floResolved).toBeTruthy();
-    expect(ltcResolved).toBeTruthy();
-    expect(bitcoinResolved).toBeTruthy();
-}, 10000);
+// test("APB, getExchangeRates(): No Coin Parameters", async () => {
+//     expect.assertions(1);
+//     await expect(APB.getExchangeRates("usd")).resolves.toMatchObject(
+//         {
+//             "flo": {"usd": expect.any(Number)},
+//             "bitcoin": {"usd": expect.any(Number)},
+//             "litecoin": {"usd": expect.any(Number)}
+//         }
+//     )
+//     // uncomment to check exchange rates:
+//     // await expect(APB.getExchangeRates("usd")).resolves.toBe()
+// }, 10000)
+//
+// test("APB, getExchangeRates(): One Coin Parameter", async () => {
+//     expect.assertions(1);
+//     await expect(APB.getExchangeRates("usd", ["flo"])).resolves.toMatchObject(
+//         {
+//             "flo": {"usd": expect.any(Number)}
+//         }
+//     )
+// }, 10000)
+//
+// test("APB, getExchangeRates(): Multiple Coin Parameters", async () => {
+//     expect.assertions(1);
+//     await expect(APB.getExchangeRates("usd", ["flo", "bitcoin", "litecoin"])).resolves.toMatchObject(
+//         {
+//             "flo": {"usd": expect.any(Number)},
+//             "bitcoin": {"usd": expect.any(Number)},
+//             "litecoin": {"usd": expect.any(Number)}
+//         }
+//     )
+// })
+//
+// test("APB, getBalances(): check to see if balances resolved", async () => {
+//     // expect.assertions(3);
+//     let balances = await APB.getBalances({discover: false});
+//
+//     let bitcoinResolved = false;
+//     let floResolved = false;
+//     let ltcResolved = false;
+//     console.log("Balances, ", balances)
+//     if (typeof balances["bitcoin"] === "number" || typeof balances["bitcoin"] === "string") {
+//         bitcoinResolved = true;
+//     }
+//     if (typeof balances["flo"] === "number" || typeof balances["flo"] === "string") {
+//         floResolved = true;
+//     }
+//     if (typeof balances["litecoin"] === "number" || typeof balances["litecoin"] === "string") {
+//         ltcResolved = true;
+//     }
+//
+//     expect(floResolved).toBeTruthy();
+//     expect(ltcResolved).toBeTruthy();
+//     expect(bitcoinResolved).toBeTruthy();
+// }, 10000);
 
 // test("APB, Pay function", async () => {
 //     let payment = await APB.pay()
@@ -66,3 +95,7 @@ test("APB, getBalances(): check to see if balances resolved", async () => {
 //
 // }, 10000)
 //
+//
+test("APB, getCoinToPayWith: ", () => {
+    APB.pay();
+})
