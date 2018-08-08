d1799964695b021dff69799e3c95b424c6f210f7
Sky Young <sky@alexandria.io>
2018-08-08 15:56:07 -0700

    Remove logging and fix test

--- test/util.test.js
+++ test/util.test.js
@@ -2,23 +2,20 @@ import { isValidEmail, isValidIdentifier } from '../src/util'
 
 test('isValidEmail', () => {
     let username = "jesus@gmail.com"
-    console.log(isValidEmail(username))
-    expect(isValidEmail(username)).toBeTruthy()
+    expect(isValidEmail(username)).toBe(true)
 })
 
 test('isNOTValidEmail', () => {
     let username = "046132a-dddc1051-cb7ec34-5692930"
-    console.log(isValidEmail(username))
-    expect(isValidEmail(username)).toBeFalsy()
+    expect(isValidEmail(username)).toBe(false)
 })
 
 test('isValidIdentifier', () => {
     let identifier = "046132a-dddc1051-cb7ec34-5692930"
-    console.log(isValidIdentifier(identifier))
-    expect(isValidIdentifier(identifier)).toBeTruthy()
+    expect(isValidIdentifier(identifier)).toBe(true)
 })
 
 test('isNOTValidIdentifier', () => {
     let username = "jesus@gmail.com"
-    console.log(isValidIdentifier(username))
+    expect(isValidIdentifier(username)).toBe(false)
 })
